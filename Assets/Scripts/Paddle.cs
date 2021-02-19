eada54bad35d85ec2046d32940b0cec628082557
alis0712 <62857780+alis0712@users.noreply.github.com>
2021-02-18 19:59:48 -0500

    Initial Commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // configuration parameters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 20.34f;
+    [SerializeField] float screenWidthInUnits = 20.34f;
+    // Start is called before the first frame update
+
+    // cached references
+    GameStatus theGameStatus;
+    Ball theBall;
+
+    void Start()
+    {
+        theGameStatus = FindObjectOfType<GameStatus>();
+        theBall = FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if(theGameStatus.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+
+    }
+}
